2124ebc1c70da109e177d45d83f5bb5c2bc4d7c0
vitusisya <vitusisya@gmail.com>
2023-03-22 00:09:41 -0700

    Put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,7 @@
-#include "putchar.h"
-/**
- * main - prints the string "Putchar" from a character array.
- * Return: 0 on success.
- */
+#include <stdio.h>
+
 int main(void)
 {
-	char c[] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
